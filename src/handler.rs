9146e63e46621ac681633b57ece2008fc5c04b42
Nessaj <ksnessaj@hotmail.com>
2026-08-16 21:00:58 +0800

    first version

--- src/handler.rs
+++ src/handler.rs
@@ -1,14 +1,37 @@
-use axum::{Json, http::StatusCode};
+use axum::{Extension, Json, http::StatusCode};
 use serde::{Deserialize, Serialize};
 
+use crate::ControlChannel;
+
+const STX: u8 = 0x02;
+const ADDR1: u8 = 0x30; // "0"
+const ADDR2: u8 = 0x31; // "1"
+const MAIN_CMD: u8 = 0x4d; // "M"
+const ETX: u8 = 0x03;
+
+fn gen_checksum(cmd: &mut Vec<u8>) {
+    let sum = cmd.iter().clone().fold(0u8,|acc, &x| acc.wrapping_add(x));
+    let sum_str = format!("{:02X}", sum);
+    let checksum = sum_str.as_bytes();
+    cmd.extend(checksum);
+    cmd.push(ETX);
+}
+
 pub async fn elevator_control(
+    Extension(control_channel): Extension<ControlChannel>,
     // this argument tells axum to parse the request body
     // as JSON into a `ElevCmd` type
     Json(command): Json<ElevCmd>,
 ) -> (StatusCode, Json<Response>) {
     // insert your application logic here
     match command.control_type {
-        ControlType::Query => {}
+        ControlType::Query => {
+            let mut cmd = vec![STX, ADDR1, ADDR2, MAIN_CMD, ControlType::Query as u8];
+            let data_vec = [0x30; 10];
+            cmd.extend(data_vec);
+            gen_checksum(&mut cmd);
+            control_channel.send(cmd);
+        }
         ControlType::Register => {}
         ControlType::Open => {}
         ControlType::Close => {}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-use axum::{
-    Router,
-    routing::{get, post},
+use axum::{Router, routing::post};
+use serial2::{
+    SerialPort,
+    rs4xx::{Rs485Config, TransceiverMode},
 };
-use std::{error::Error, sync::{Arc, Mutex, mpsc::{channel, Sender, Receiver}}};
+use std::{
+    error::Error,
+    sync::{
+        Arc, Mutex,
+        mpsc::{Sender, channel},
+    },
+    thread,
+};
 
 use crate::handler::elevator_control;
 // use tracing_subscriber::fmt::format::FmtSpan;
-// use tracing::{error, info};
+use tracing::{error, info};
 
 mod handler;
 
@@ -14,6 +22,13 @@ mod handler;
 pub struct ControlChannel {
     // Wrap mutable values in a thread-safe container like Arc + Mutex/RwLock
     sender: Arc<Mutex<Sender<Vec<u8>>>>,
+}
+
+impl ControlChannel {
+    pub fn send(&self, cmd: Vec<u8>) {
+        let tx = self.sender.lock().unwrap();
+        let _ = tx.send(cmd);
+    }
 }
 
 #[tokio::main]
@@ -27,11 +42,50 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
         .init();
 
     let (tx, rx) = channel::<Vec<u8>>();
+    let port = SerialPort::open("/dev/ttyUSB0", 9600)?;
+
+    // Configure RS-485 transceiver mode (Linux only)
+    // This enables the RS485 mode structure in the underlying OS driver
+    let rs485conf = Rs485Config::new();
+    let mode = TransceiverMode::Rs485(rs485conf);
+
+    if let Err(e) = port.set_rs4xx_mode(mode) {
+        error!(
+            "Note: Software RS485 configuration not supported or failed on this hardware/driver: {e}"
+        );
+        return Err(e.into());
+    }
+    // Spawn an std thread to handle channel receiver
+    thread::spawn(move || {
+        while let Ok(cmd) = rx.recv() {
+            if let Err(e) = port.write(&cmd) {
+                error!("Failed to send command {cmd:?} to elevator! Error: {e:?}");
+            }
+            if let Err(e) = port.flush() {
+                error!("Failed to flush! Error: {e:?}");
+            }
+            let mut buffer = [0u8; 64];
+            match port.read(&mut buffer) {
+                Ok(0) => (),
+                Ok(s) => {
+                    info!("{s} bytes received after sending elevator command.");
+                }
+                Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => {
+                    error!("Error: Read timed out. No response from RS-485 device.");
+                }
+                Err(e) => {
+                    error!("Error reading from port: {:?}", e);
+                }
+            }
+        }
+        info!("Channel closed");
+    });
+
     let sender = Arc::new(Mutex::new(tx));
     // build our application with a route
     let app = Router::new()
         // `GET /` goes to `root`
-        .route("/", get(root))
+        // .route("/", get(root))
         // `POST /elevctrl` goes to `elevator_control`
         .route("/elevctrl", post(elevator_control))
         .with_state(sender);
@@ -43,6 +97,6 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
 }
 
 // basic handler that responds with a static string
-async fn root() -> &'static str {
-    "Hello, World!"
-}
+// async fn root() -> &'static str {
+//     "Hello, World!"
+// }
